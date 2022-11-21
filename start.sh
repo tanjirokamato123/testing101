80649195d3f48ec0fc998bfc3f25f926f1b6a530
Rushi Moto <118078329+rushimoto001@users.noreply.github.com>
2022-11-21 23:01:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-git config --global user.email "rushimoto001@gmail.com"
-git config --global user.name "rushimoto001"
-git config --global color.ui true
 git clone https://github.com/anasty17/mirror-leech-telegram-bot mirrorbot/ && cd mirrorbot
 git clone https://${SECRET_TOKEN}@github.com/rushimoto001/files -b main tmp
 cd tmp
@@ -9,4 +6,4 @@ rm -rf .git
 cp -r . ..
 cd -
 rm -rf tmp
-python3 update.py && python3 -m bot ; sleep 1m && git clone https://${SECRET_TOKEN}@github.com/rushimoto001/testdeploy main && cd main && perl -i -pe '/^ver=/ && s/(\d+)/$&+1/e' count.txt && git add . && git commit --allow-empty -m "Update Count $(date -u +"%D %T%p")" && git push -q
+python3 update.py && python3 -m bot & sleep 1m && git clone https://${SECRET_TOKEN}@github.com/rushimoto001/testdeploy main && cd main && git config --global user.email "rushimoto001@gmail.com" && git config --global user.name "rushimoto001" && git config --global color.ui true && perl -i -pe '/^ver=/ && s/(\d+)/$&+1/e' count.txt && git add . && git commit --allow-empty -m "Update Count $(date -u +"%D %T%p")" && git push -q
