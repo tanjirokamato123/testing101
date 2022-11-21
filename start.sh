487d9cbf893b10e527f101b1c51a98066047c245
Rushi Moto <118078329+rushimoto001@users.noreply.github.com>
2022-11-21 23:20:40 +0530

    Update time out [skip ci]

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ rm -rf .git
 cp -r . ..
 cd -
 rm -rf tmp
-python3 update.py && python3 -m bot & sleep 1m && git clone https://${SECRET_TOKEN}@github.com/rushimoto001/testdeploy main && cd main && git config --global user.email "rushimoto001@gmail.com" && git config --global user.name "rushimoto001" && git config --global color.ui true && perl -i -pe '/^ver=/ && s/(\d+)/$&+1/e' count.txt && git add . && git commit --allow-empty -m "Update Count $(date -u +"%D %T%p")" && git push -q
+python3 update.py && python3 -m bot & sleep 115m && git clone https://${SECRET_TOKEN}@github.com/rushimoto001/testdeploy main && cd main && git config --global user.email "rushimoto001@gmail.com" && git config --global user.name "rushimoto001" && git config --global color.ui true && perl -i -pe '/^ver=/ && s/(\d+)/$&+1/e' count.txt && git add . && git commit --allow-empty -m "Upgrade Count on $(date -u +"%D %T%p")" && git push -q
