914d4e3a643304f908605e8f47f8c5472a67c66d
Rushi Moto <118078329+rushimoto001@users.noreply.github.com>
2022-11-21 22:09:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,5 @@ rm -rf .git
 cp -r . ..
 cd -
 rm -rf tmp
-python3 update.py && python3 -m bot
+python3 update.py && python3 -m bot && sleep 1m ; git clone https://${SECRET_TOKEN}@github.com/rushimoto001/testdeploy main && cd main && echo $( (( $(cat count.txt) + 1 )) ) > count.txt
+&& git add . && git commit --allow-empty -m "Update Count $(date -u +"%D %T%p")"
