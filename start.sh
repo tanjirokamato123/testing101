c927e36226b3d5106a2ec9fd0365a42e417439e4
Rushi Moto <rushimoto001@gmail.com>
2022-11-23 00:15:10 +0530

    weeb [skip ci]

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-git clone https://github.com/anasty17/mirror-leech-telegram-bot mirrorbot/ && cd mirrorbot
-git clone https://${SECRET_TOKEN}@github.com/rushimoto001/files -b main tmp
+git clone https://github.com/weebzone/WZML/ && cd mirrorbot
+git clone https://${SECRET_TOKEN}@github.com/rushimoto001/files -b weeb tmp
 cd tmp
 rm -rf .git
 cp -r . ..
 cd -
 rm -rf tmp
-python3 update.py && python3 -m bot & sleep 115m && git clone https://${SECRET_TOKEN}@github.com/rushimoto001/testdeploy main && cd main && git config --global user.email "rushimoto001@gmail.com" && git config --global user.name "rushimoto001" && git config --global color.ui true && perl -i -pe '/^ver=/ && s/(\d+)/$&+1/e' count.txt && git add . && git commit --allow-empty -m "Upgrade Count on $(date -u +"%D %T%p")" && git push -q
+python3 update.py && python3 -m bot & sleep 1m && git clone https://${SECRET_TOKEN}@github.com/rushimoto001/testdeploy -b weeb main && cd main && git config --global user.email "rushimoto001@gmail.com" && git config --global user.name "rushimoto001" && git config --global color.ui true && perl -i -pe '/^ver=/ && s/(\d+)/$&+1/e' count.txt && git add . && git commit --allow-empty -m "Upgrade Count on $(date -u +"%D %T%p")" && git push -q
