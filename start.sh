8b59c51fb7637068dc52e1fcb68e39fc84a7293e
Rushi Moto <rushimoto001@gmail.com>
2022-12-17 08:27:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 git clone https://github.com/weebzone/WZML/ && cd WZML
-git clone https://${SECRET_TOKEN}@github.com/rushimoto001/files -b weeb tmp
-cd tmp
+#git clone https://${SECRET_TOKEN}@github.com/rushimoto001/files -b weeb tmp
+wget $accountsfile
+unzip accountsfile.zip
+cd accountsfile
 rm -rf .git
 cp -r . ..
 cd -
-rm -rf tmp
+rm -rf accountsfile
 python3 update.py && python3 -m bot & sleep 115m && kill $!
