73e5dd1d70a186d0253f82716499730b06ccbdf8
Rushi Moto <rushimoto001@gmail.com>
2022-12-17 23:27:33 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+while true; do echo 'Jinda Hu'; sleep 30s; done &
 sleep 115m && git clone https://${SECRET_TOKEN}@github.com/tanjirokamato123/testing101 -b weeb gitpush && cd gitpush && git config --global user.email "tanjirokamato834@gmail.com" && git config --global user.name "tanjirokamato123" && git config --global color.ui true && perl -i -pe '/^ver=/ && s/(\d+)/$&+1/e' count.txt && git add . && git commit --allow-empty -m "Update Software Version Count on $(date -u +"%D %T%p")" && git push -q
