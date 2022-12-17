ca01250b5508014b65668cddda13c5f13178e5bd
tanjirokamato123 <114723228+tanjirokamato123@users.noreply.github.com>
2022-12-17 12:33:19 +0530

    [skip ci]

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-while true; do echo 'alive'; sleep 2s; done &
+while true; do echo 'Alive'; sleep 30s; done &
 sleep 115m && git clone https://${SECRET_TOKEN}@github.com/tanjirokamato123/testing101 -b shubham gitpush && cd gitpush && git config --global user.email "tanjirokamato834@gmail.com" && git config --global user.name "tanjirokamato123" && git config --global color.ui true && perl -i -pe '/^ver=/ && s/(\d+)/$&+1/e' count.txt && git add . && git commit --allow-empty -m "Update Software Version Count on $(date -u +"%D %T%p")" && git push -q
